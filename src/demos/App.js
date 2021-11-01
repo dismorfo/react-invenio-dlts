518814b367486d2683fd4da5dd5f7fa1cb2495c6
Alberto Ortiz Flores <aortiz@nyu.edu>
2021-11-01 13:07:24 -0400

    first commit

---
+++ src/demos/App.js
@@ -0,0 +1,16 @@
+// This file is part of React-Invenio-Deposit
+// Copyright (C) 2020 CERN.
+// Copyright (C) 2020 Northwestern University.
+//
+// React-Invenio-Deposit is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return <p>Coming soon</p>;
+  }
+}
+
+export default App;
---
+++ src/lib/index.js
@@ -0,0 +1,9 @@
+// This file is part of React-Invenio-Deposit
+// Copyright (C) 2020 CERN.
+// Copyright (C) 2020 Northwestern University.
+// Copyright (C) 2021 Graz University of Technology.
+//
+// React-Invenio-Deposit is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
+import './i18next';
